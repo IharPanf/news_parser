a8947b53b28a498e92dc9d9cc9d20e6645d49393
Igor Panfilenko <ipanfilenko@encore.co.uk>
2017-09-04 20:51:52 +0300

    Show error on lost connection

--- front/src/app/components/root/app.component.ts
+++ front/src/app/components/root/app.component.ts
@@ -6,6 +6,7 @@ import { ApiService } from './../../services/api.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Self education: Парсер новостей (Mongo, NodeJS, Angular)';
   lat = 53.9195866;
@@ -17,19 +18,26 @@ export class AppComponent {
   locLat = '';
   locLng = '';
   emptyResults = '';
-  
+  errorStr = '';
   constructor(private apiService: ApiService) {}
 
   /**
    * 
    */
-  public getAllNews() {
-    this.apiService.getAllNews().subscribe((res) => {
-      if (res) {
-        this.listOfNews = res.json();
-        this.listOfWords = [];
-      }
-    });
+  public getAllNews(): void {
+    this.setInitialValue();
+    this.apiService.getAllNews().subscribe(
+        (res) => {
+          if (res) {
+            this.listOfNews = res.json();
+            this.listOfWords = [];
+          }
+        },
+        (err) => {
+          this.errorStr = 'Error connection to server..';
+          console.log(this.errorStr, err);
+        }
+    );
   }
 
   /**
@@ -37,21 +45,27 @@ export class AppComponent {
    * @param location
    * @param distance
    */
-  public getCoordinates(location: string, distance) {
+  public getCoordinates(location: string, distance: any): void {
+    this.setInitialValue();
     this.distance = distance;
     if (location) {
-      this.apiService.getCoordinates(location).subscribe((res) => {
-        if (res && res.results && res.results[0].geometry && res.results[0].geometry.location) {
-          this.locLat = res.results[0].geometry.location.lat;
-          this.locLng = res.results[0].geometry.location.lng;
+      this.apiService.getCoordinates(location).subscribe(
+        (res) => {
+          if (res && res.results && res.results[0].geometry && res.results[0].geometry.location) {
+            this.locLat = res.results[0].geometry.location.lat;
+            this.locLng = res.results[0].geometry.location.lng;
           
-          if (distance) {
-            this.getNearNews(this.locLat, this.lng, distance);
+            if (distance) {
+              this.getNearNews(this.locLat, this.locLng, distance);
+            }
           }
+        },
+        (err) => {
+          this.errorStr = 'Error connection to server..';
+          console.log(this.errorStr, err);
         }
-      });
+      );
     }
-    
   }
 
   /**
@@ -60,28 +74,50 @@ export class AppComponent {
    * @param lng
    * @param distance
    */
-  public getNearNews(lat, lng, distance) {
-    this.apiService.getNearNews(lat, lng, distance).subscribe((res) => {
-      if (res) {
-        this.listOfNews = res.json();
-        this.listOfWords = [];
-        this.emptyResults = '';
-        if (this.listOfNews.length === 0) {
-          this.emptyResults = 'Нет новостей в заданном радиусе от выбранной точки';
-        } 
-      } 
-    });
+  public getNearNews(lat: string, lng: string, distance: any): void {
+    this.setInitialValue();
+    this.apiService.getNearNews(lat, lng, distance).subscribe(
+      (res) => {
+        if (res) {
+          this.listOfNews = res.json();
+          this.listOfWords = [];
+          this.emptyResults = '';
+          if (this.listOfNews.length === 0) {
+            this.emptyResults = 'Нет новостей в заданном радиусе от выбранной точки';
+          }
+        }
+      },
+      (err) => {
+        this.errorStr = 'Error connection to server..';
+        console.log(this.errorStr, err);
+      }
+    );
   }
 
   /**
    * 
    */
-  public countWords() {
-    this.apiService.countWords().subscribe((res) => {
-      if (res) {
-        this.listOfWords = res.json();
-        console.log(this.listOfWords);
+  public countWords(): void {
+    this.setInitialValue();
+    this.apiService.countWords().subscribe(
+      (res) => {
+        if (res) {
+          this.listOfWords = res.json();
+        }
+      },
+      (err) => {
+        this.errorStr = 'Error connection to server..';
+        console.log(this.errorStr, err);
       }
-    });
+    );
+  }
+
+  /**
+   * 
+   */
+  private setInitialValue(): void {
+    this.errorStr = '';
+    this.listOfNews = [];
+    this.listOfWords = [];
   }
 }
